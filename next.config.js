0e95be61bcaf6288c1fead2b385d32eaa037e887
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-24 19:31:21 +0000

    changes

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports = {
+  // Specify the configuration options
+  // For static HTML export
+  output: {
+    // Set the export target to static HTML
+    // This replaces the need for the `next export` command
+    // Learn more: https://nextjs.org/docs/advanced-features/static-html-export
+    export: true,
+  },
+};
