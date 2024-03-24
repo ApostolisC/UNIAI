690450c021d308154c5eb89723ed72bd0b3b2b7e
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-24 21:03:42 +0000

    about endpoint

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+// pages/about.tsx
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the About page content.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
