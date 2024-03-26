bd7e0d60bee3794274a8b68395a89cfcf11b4eb7
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-26 02:04:55 +0000

    15.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  // Set the base path for the Next.js application
-  basePath: isProd ? '/UNIAI' : '',
-
-  // Set the asset prefix for the Next.js application
-  assetPrefix: isProd ? '/UNIAI/' : '',
-
-  // Configure Next.js to export static HTML
-  // Learn more: https://nextjs.org/docs/advanced-features/static-html-export
-  output: 'export',
-};
+module.export = {
+  output: "export",
+  // ...
+}
