129422baf11770bdc0208fce960bc2e54c3cd784
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-26 00:47:26 +0000

    ..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from './index';
+import HomePage from './index'; // Adjust the path accordingly
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
