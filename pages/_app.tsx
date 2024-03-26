7a3c3f9ea424c8ce299dd2a0ed2b23e450751e00
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-26 00:48:46 +0000

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import HomePage from './index'; // Adjust the path accordingly
+import { AppProps } from 'next/app';
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
