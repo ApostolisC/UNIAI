5bc8ea79adb463de6fd27a29ee58844968aec779
Apostolos Chasiotis <apostolis10@pm.me>
2024-03-31 04:34:25 +0000

    modified:   components/Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -94,7 +94,7 @@ const Navigation = () => {
               </Link>
             </li>
             <li>
-              <Link href="/UNIAI/become-a-partner">
+              <Link href="/become-a-partner">
                 Γίνε Συνεργάτης
               </Link>
             </li>
