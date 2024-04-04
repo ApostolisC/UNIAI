60d0f17c5b664b09c5ba689815d95134b786f3fd
Apostolos Chasiotis <apostolis10@pm.me>
2024-04-04 06:13:31 +0000

    Transfer main content from navigation to index

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -8,24 +8,6 @@ import React, { useEffect, useState } from 'react';
 
 const Navigation = () => {
 
-  useEffect(() => {
-    // JavaScript code to be executed after component mounts
-    const image1 = document.querySelector(`.${styles.image1}`) as HTMLElement;
-    const image2 = document.querySelector(`.${styles.image2}`) as HTMLElement;
-    if (image1){
-      setTimeout(() => {
-        image1.style.opacity = '1'; // Set opacity to 1i
-        image1.style.transition = 'opacity 1s ease-in-out';
-      }, 1000);
-    }
-    if (image2){
-      setTimeout(() => {
-        image2.style.opacity = '1'; // Set opacity to 1
-        image2.style.transition = 'opacity 1s ease-in-out';
-      }, 2000);
-    }
-  }, []);
-
   const [showSubMenu, setShowSubMenu] = useState(false);
 
   return (
@@ -88,11 +70,6 @@ const Navigation = () => {
         </ul>
       </div>
 
-      <div className={`${styles.image_container} ${styles.hidden}`}>
-        <img className={styles.image1} src="/wires.png" alt="Logo"/>
-        <img className={styles.image2} src="/logo2.png" alt="Logo"/>
-
-      </div>
 
     </header>
   );
