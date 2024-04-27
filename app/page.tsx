4d439d5e1e752a03c3b06ce8de4b338af1f9ecfa
Apostolos Chasiotis <apostolis10@pm.me>
2024-04-27 15:19:27 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@ import '../styles/globals.css';
 import Image from 'next/image';
 import styles from '../styles/page.module.css';
 
-const HomePage = () => {
+export default function Page() {
   return (
-      <div className="relative my-auto mx-auto">
-          <div className="relative h-screen">
+    <div className="relative my-auto mx-auto">
+          <div className="relative">
               <Image src="/wires.png" alt="Background Image" layout="fill" objectFit="contain"/>
               <Image src="/logo2.png" alt="Overlay Image" className="" layout="fill" objectFit="none"/>
           </div>
           <p>test</p>
-      </div>
-
-  )
-      ;
+    </div>
+  );
 };
-
-export default HomePage;
